c38377c3472effcb87d463546d3ace94fb5a80bc
Mitchell1994 <mitchell.longair1@gmail.com>
2016-03-07 14:04:58 +1300

    Fixed IP switching bug. Output now prints like log entries.

--- AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/MainActivity.java
+++ AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/MainActivity.java
@@ -3,6 +3,7 @@ package com.chortle.chortle;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -18,59 +19,64 @@ public class MainActivity extends AppCompatActivity {
     // Instantiate the RequestQueue.
     RequestQueue queue;
     String url = "http://" + UrlValues.MitchellIP + ":" + UrlValues.Port + "/chortleservice/users";
+    int count = 1;
 
     User user = new User("a","b","c","d","e");
 
-    JsonObjectRequest request = new JsonObjectRequest(JsonObjectRequest.Method.POST,
-        url, user.toJson(),
-        new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                TextView mTextView = (TextView) findViewById(R.id.output);
-                mTextView.setText( mTextView.getText() + "Got a response");
-            }
-        }, new Response.ErrorListener() {
-
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            TextView mTextView = (TextView) findViewById(R.id.output);
-            mTextView.setText(mTextView.getText() + "That didn't work!" + error.toString());
-        }
-    }
-    );
-
-    private View.OnClickListener addUserListener = new View.OnClickListener() {
+    private View.OnClickListener buttonListener = new View.OnClickListener() {
         public void onClick(View v) {
+            TextView mTextView = (TextView) findViewById(R.id.output);
+            String ip = UrlValues.MitchellIP;
             switch (v.getId()) {
                 case R.id.addUser:
-                    // Add the request to the RequestQueue.*/
+                    print("Contacting " + url);
+                    JsonObjectRequest request = new JsonObjectRequest(JsonObjectRequest.Method.POST,
+                            url, user.toJson(),
+                        new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                TextView mTextView = (TextView) findViewById(R.id.output);
+                                print("Success");
+                            }
+                        }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            TextView mTextView = (TextView) findViewById(R.id.output);
+                            print("Failure (" + error.toString() + ")");
+                            count++;
+                        }
+                    }
+                    );
                     queue.add(request);
+                    break;
+                case R.id.radio_mitchell:
+                    ip = UrlValues.MitchellIP;
+                    break;
+                case R.id.radio_conor:
+                    ip = UrlValues.ConorIP;
+                    break;
+                case R.id.radio_gianni:
+                    ip = UrlValues.GianniIP;
+                    break;
                 default:
                     break;
             }
-        }
-    };
-
-    private View.OnClickListener ipSelectorListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            boolean checked = ((RadioButton) v).isChecked();
-            switch(v.getId()) {
-                case R.id.radio_mitchell:
-                    if (checked)
-                        url = "http://" + UrlValues.MitchellIP + ":" + UrlValues.Port + "/chortleservice/users";
-                    break;
-                case R.id.radio_conor:
-                    if (checked)
-                        url = "http://" + UrlValues.ConorIP + ":" + UrlValues.Port + "/chortleservice/users";
-                    break;
-                case R.id.radio_gianni:
-                    if (checked)
-                        url = "http://" + UrlValues.GianniIP + ":" + UrlValues.Port + "/chortleservice/users";
-                    break;
+            if(v.getId() == R.id.radio_mitchell
+                    || v.getId() == R.id.radio_conor
+                    || v.getId() == R.id.radio_gianni){
+                url = "http://" + ip + ":" + UrlValues.Port + "/chortleservice/users";
+                print("IP set to " + ip);
             }
         }
     };
+
+private void print(String msg) {
+    TextView mTextView = (TextView) findViewById(R.id.output);
+    mTextView.append("\n" + count + ". " + msg);
+    count++;
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,14 +85,30 @@ public class MainActivity extends AppCompatActivity {
         queue = Volley.newRequestQueue(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Set up add user button
         Button addUser = (Button)findViewById(R.id.addUser);
-        addUser.setOnClickListener(addUserListener);
+        addUser.setOnClickListener(buttonListener);
+
+        //Create radio buttons
         RadioButton mitchellIP = (RadioButton)findViewById(R.id.radio_mitchell);
-        mitchellIP.setChecked(true);
-        mitchellIP.setOnClickListener(ipSelectorListener);
         RadioButton conorIP = (RadioButton)findViewById(R.id.radio_conor);
-        conorIP.setOnClickListener(ipSelectorListener);
         RadioButton gianniIP = (RadioButton)findViewById(R.id.radio_gianni);
-        gianniIP.setOnClickListener(ipSelectorListener);
+
+        //Set radio button listeners
+        mitchellIP.setOnClickListener(buttonListener);
+        conorIP.setOnClickListener(buttonListener);
+        gianniIP.setOnClickListener(buttonListener);
+
+        //Set radio button defualts
+        mitchellIP.setChecked(true);
+        conorIP.setChecked(false);
+        gianniIP.setChecked(false);
+
+        //set up output
+        TextView mTextView = (TextView) findViewById(R.id.output);
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        print("IP set to " + UrlValues.MitchellIP);
     }
 }
--- AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/UrlValues.java
+++ AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/UrlValues.java
@@ -7,5 +7,5 @@ public final class UrlValues {
     public static String Port = "9910";
     public static String MitchellIP = "192.168.20.5";
     public static String ConorIP = "192.168.1.66";
-    public static String GianniIP = "";
+    public static String GianniIP = "Gianni";
 }

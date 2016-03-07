7aea8f906ba3e3154f36822c8d5e8f38ffe8bd4c
Mitchell1994 <mitchell.longair1@gmail.com>
2016-03-07 23:59:09 +1300

    Added onClickListener to online-host toggle in the Android app.

--- AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/MainActivity.java
+++ AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/MainActivity.java
@@ -31,63 +31,55 @@ public class MainActivity extends AppCompatActivity {
     String url, ip;
     int count = 1;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Define default url
+        url = "http://" + getString(R.string.mitchell_ip) + ":" + getString(R.string.port) + "/chortleservice/users";
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Set up add user button
+        Button add_user = (Button)findViewById(R.id.add_user);
+        add_user.setOnClickListener(buttonListener);
+
+        //Create radio buttons
+        RadioButton mitchellIP = (RadioButton)findViewById(R.id.radio_mitchell);
+        RadioButton conorIP = (RadioButton)findViewById(R.id.radio_conor);
+        RadioButton gianniIP = (RadioButton)findViewById(R.id.radio_gianni);
+        RadioButton radioIP = (RadioButton)findViewById(R.id.radio_online_host);
+
+        //Set radio button listeners
+        mitchellIP.setOnClickListener(buttonListener);
+        conorIP.setOnClickListener(buttonListener);
+        gianniIP.setOnClickListener(buttonListener);
+        radioIP.setOnClickListener(buttonListener);
+
+        //Set radio button defualts
+        mitchellIP.setChecked(true);
+        conorIP.setChecked(false);
+        gianniIP.setChecked(false);
+
+        //set up output
+        TextView mTextView = (TextView) findViewById(R.id.output);
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        //Set up default IP
+        ip = getString(R.string.mitchell_ip);
+        print("IP set to " + ip);
+    }
+
     private View.OnClickListener buttonListener = new View.OnClickListener() {
         public void onClick(View v) {
             TextView mTextView = (TextView) findViewById(R.id.output);
             ip = getString(R.string.mitchell_ip);
             switch (v.getId()) {
                 case R.id.add_user:
-                    //get username and email
-                    String username = ((EditText) findViewById(R.id.username)).getText().toString();
-                    String email = ((EditText) findViewById(R.id.email)).getText().toString();
-                    User user = new User(username,"fname","lname", email,"hash");
-
-                    print("Contacting " + url);
-                    Gson gson = new Gson();
-                    JSONObject json;
-                    try {
-                        json = new JSONObject(gson.toJson(user));
-                    }catch(JSONException e){
-                        json = null;
-                    }
-                    final JsonObjectRequest request = new JsonObjectRequest(JsonObjectRequest.Method.POST,
-                            url,json,
-                        new Response.Listener<JSONObject>() {
-
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                TextView mTextView = (TextView) findViewById(R.id.output);
-                                print("Success");
-                            }
-                        }, new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                TextView mTextView = (TextView) findViewById(R.id.output);
-                                print("Failure (" + error.networkResponse.statusCode + ")");
-
-                               /* //TESTING
-
-                                NetworkResponse response = error.networkResponse;
-                                Map<String, String> responseHeaders = response.headers;
-                                Response<String> result = Response.success(responseHeaders.get("Content-Length"), HttpHeaderParser.parseCacheHeaders(response));
-
-                                //Test 2
-                                try {
-                                    String responseBody = new String( error.networkResponse.data, "utf-8" );
-                                    JSONObject jsonObject = new JSONObject( responseBody );
-                                    print(jsonObject.toString());
-                                } catch (UnsupportedEncodingException e) {
-                                    e.printStackTrace();
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }*/
-
-                            }
-                        }
-                    );
-                    print("Sending " + new String(request.getBody()));
-                    queue.add(request);
+                    addUser();
                     break;
                 case R.id.radio_mitchell:
                     ip = getString(R.string.mitchell_ip);
@@ -114,49 +106,64 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-private void print(String msg) {
-    TextView mTextView = (TextView) findViewById(R.id.output);
-    mTextView.append(count + ". " + msg + "\n");
-    count++;
-}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        //Define default url
-        url = "http://" + getString(R.string.mitchell_ip) + ":" + getString(R.string.port) + "/chortleservice/users";
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        queue = Volley.newRequestQueue(this);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        //Set up add user button
-        Button add_user = (Button)findViewById(R.id.add_user);
-        add_user.setOnClickListener(buttonListener);
-
-        //Create radio buttons
-        RadioButton mitchellIP = (RadioButton)findViewById(R.id.radio_mitchell);
-        RadioButton conorIP = (RadioButton)findViewById(R.id.radio_conor);
-        RadioButton gianniIP = (RadioButton)findViewById(R.id.radio_gianni);
-
-        //Set radio button listeners
-        mitchellIP.setOnClickListener(buttonListener);
-        conorIP.setOnClickListener(buttonListener);
-        gianniIP.setOnClickListener(buttonListener);
-
-        //Set radio button defualts
-        mitchellIP.setChecked(true);
-        conorIP.setChecked(false);
-        gianniIP.setChecked(false);
-
-        //set up output
+    private void print(String msg) {
         TextView mTextView = (TextView) findViewById(R.id.output);
-        mTextView.setMovementMethod(new ScrollingMovementMethod());
+        mTextView.append(count + ". " + msg + "\n");
+        count++;
+    }
 
-        //Set up default IP
-        ip = getString(R.string.mitchell_ip);
-        print("IP set to " + ip);
+    private void addUser(){
+        //get username and email
+        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+        String email = ((EditText) findViewById(R.id.email)).getText().toString();
+        User user = new User(username,"fname","lname", email,"hash");
+
+        print("Contacting " + url);
+        Gson gson = new Gson();
+        JSONObject json;
+        try {
+            json = new JSONObject(gson.toJson(user));
+        }catch(JSONException e){
+            json = null;
+        }
+        final JsonObjectRequest request = new JsonObjectRequest(JsonObjectRequest.Method.POST,
+                url,json,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        TextView mTextView = (TextView) findViewById(R.id.output);
+                        print("Success");
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView mTextView = (TextView) findViewById(R.id.output);
+                print("Failure (" + error.networkResponse.statusCode + ")");
+
+                               /* //TESTING
+
+                                NetworkResponse response = error.networkResponse;
+                                Map<String, String> responseHeaders = response.headers;
+                                Response<String> result = Response.success(responseHeaders.get("Content-Length"), HttpHeaderParser.parseCacheHeaders(response));
+
+                                //Test 2
+                                try {
+                                    String responseBody = new String( error.networkResponse.data, "utf-8" );
+                                    JSONObject jsonObject = new JSONObject( responseBody );
+                                    print(jsonObject.toString());
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }*/
+
+            }
+        }
+        );
+        print("Sending " + new String(request.getBody()));
+        queue.add(request);
     }
 }

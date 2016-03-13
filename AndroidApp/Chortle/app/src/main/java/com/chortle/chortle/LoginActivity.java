001d63e190415982c9a1ee05af4d52f21239a47d
conorking <conorking10@gmail.com>
2016-03-13 15:35:46 +1300

    Added new Login activity

---
+++ AndroidApp/Chortle/app/src/main/java/com/chortle/chortle/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.chortle.chortle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void attemptLogin(View v){
+        Toast.makeText(LoginActivity.this, "Attempting login", Toast.LENGTH_SHORT).show();
+    }
+}
